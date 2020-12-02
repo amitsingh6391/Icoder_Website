21c862fb51f0b351d9ad5d5c9d3532c8d61ba1ae
amit babu <amitsingh506142@gmail.com>
2020-12-03 03:42:16 +0530

    admin pannel and contact us complete

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from home.models import Contact
+
 # Register your models here.
+
+admin.site.register(Contact)
+
+
+
+
--- home/views.py
+++ home/views.py
@@ -3,6 +3,8 @@
 # Create your views here.
 from django.shortcuts import render,HttpResponse
 
+from home.models import Contact
+
 
 
 # Create your views here.
@@ -15,4 +17,20 @@ def about(request):
     #return HttpResponse("this is about")
 def contact(request):
     #return HttpResponse("this is contact")
-     return render(request,'home/contact.html',)
+
+    if request.method=="POST":
+
+        
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        content = request.POST['content']
+
+        print(name,email,phone,content)
+
+        contact = Contact(name=name,email=email,phone=phone,content=content)
+        contact.save()
+      
+    
+
+    return render(request,'home/contact.html',)
